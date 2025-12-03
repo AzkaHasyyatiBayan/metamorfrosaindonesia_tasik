ea1e181bf285c9edc54dec75e268a4db1956723a
lucentc <150416567+lucentc@users.noreply.github.com>
2025-12-03 20:54:59 +0700

    update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.bing.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.imgur.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.pixabay.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
